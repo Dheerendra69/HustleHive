b5808612f06cc09fc61d93708eb95413b2c62ea2
Thibault <melkir13@gmail.com>
2025-01-23 22:20:38 +0900

    Fix schema generation
    This is preventing the schema generation 
    ```
    ❯ npx sanity@latest schema extract
    ✗ Failed to extract schema
    ```

--- sanity/schemaTypes/author.ts
+++ sanity/schemaTypes/author.ts
@@ -1,5 +1,5 @@
 import { defineField, defineType } from "sanity";
-import { UserIcon } from "lucide-react";
+import { UserIcon } from "@sanity/icons";
 
 export const author = defineType({
   name: "author",
